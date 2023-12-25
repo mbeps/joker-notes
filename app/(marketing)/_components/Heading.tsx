cadd98a3a868377037a12df9491452c613f98e71
Maruf Bepary <bepary71@gmail.com>
2023-12-25 18:16:07 +0000

    Implemented authentication with Clerk - Landing page can adapt using the auth state

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/Spinner/Spinner";
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -15,13 +20,27 @@ export const Heading = () => {
         Motion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-
-      <Button asChild>
-        <Link href="/documents">
-          Enter Motion
-          <ArrowRight className="h-4 w-4 ml-2" />
-        </Link>
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Motion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Motion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
