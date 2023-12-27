eea762296fec9649713404c217d5c0260fd8515c
Maruf Bepary <bepary71@gmail.com>
2023-12-27 15:46:42 +0000

    BUG FIX: Fix Convex auth error - Caused by having search in global layout instead of `main` layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "@/components/Spinner/Spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import Navigation from "./_components/Navigation";
+import SearchCommand from "@/components/Search/SearchCommand";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -25,7 +26,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
-        {/* <SearchCommand /> */}
+        <SearchCommand />
         {children}
       </main>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
+import { ConvexClientProvider } from "@/providers/ConvexProvider";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/providers/ThemeProvider";
-import { ConvexClientProvider } from "@/providers/ConvexProvider";
 import { Toaster } from "sonner";
-import SearchCommand from "@/components/Search/SearchCommand";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,7 +29,6 @@ export default function RootLayout({
             storageKey="motion-theme"
           >
             <Toaster />
-            <SearchCommand />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
