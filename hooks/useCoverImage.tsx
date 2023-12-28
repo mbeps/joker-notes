f0acbf1b297d2161c28e77ce681b22f69ff0f85b
Maruf Bepary <bepary71@gmail.com>
2023-12-28 19:36:09 +0000

    DOC: Added documentation for the hooks

--- hooks/useCoverImage.tsx
+++ hooks/useCoverImage.tsx
@@ -8,6 +8,16 @@ type CoverImageStore = {
   onReplace: (url: string) => void;
 };
 
+/**
+ * Manages the state of the cover image modal.
+ * - url (string) - the url of the cover image.
+ * - isOpen (boolean) - whether the cover image modal is open (true) or not (false).
+ * - onOpen (function) - opens the cover image modal.
+ * - onClose (function) - closes the cover image modal.
+ * - onReplace (function) - replaces the cover image.
+ * @returns (object) - the state of the cover image modal.
+ * @see https://docs.pmnd.rs/zustand/getting-started/introduction#first-create-a-store
+ */
 export const useCoverImage = create<CoverImageStore>((set) => ({
   url: undefined,
   isOpen: false,
--- hooks/useOrigin.tsx
+++ hooks/useOrigin.tsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react";
 
 // gets the origin of the current window so the host name
+/**
+ * Fetches the host name of the current window.
+ * This is useful when using the app in different environments.
+ * @returns (string) - the origin of the current window (host name)
+ */
 export const useOrigin = () => {
   const [mounted, setMounted] = useState(false);
+
+  /**
+   * Gets the origin of the current window.
+   */
   const origin =
     typeof window !== "undefined" && window.location.origin
       ? window.location.origin
