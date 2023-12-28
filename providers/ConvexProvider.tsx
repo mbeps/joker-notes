25e9e739bf7364f1b92dff1401cd137adafecc80
Maruf Bepary <bepary71@gmail.com>
2023-12-28 19:24:04 +0000

    DOC: Added documentation to providers

--- providers/ConvexProvider.tsx
+++ providers/ConvexProvider.tsx
@@ -7,6 +7,15 @@ import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
+/**
+ * Convex Provider to be used in the global Layout component.
+ * This allows the Convex client and Clerk to be managed throughout the entire app.
+ * This means that the Convex client and Clerk will be available in all components,
+ * giving access to the database and authentication.
+ * @param children (React.ReactNode) - the rest of the app.
+ * @returns (JSX.Element) - Convex Provider (with Clerk)
+ * @see https://clerk.com/docs/integrations/databases/convex
+ */
 export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
   return (
     <ClerkProvider
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -4,14 +4,22 @@ import CoverImageModal from "@/components/Modals/CoverImageModal";
 import SettingsModal from "@/components/Modals/SettingsModal";
 import { useEffect, useState } from "react";
 
-// prevents any modals from rendering on the server
+/**
+ * Global Modal Provider to be used in the global Layout component.
+ * This allows modals defined in the components/Modals folder to be rendered on the client side only throughout the entire app.
+ * The modal cannot the rendered on the server side because it requires the window object which will cause hydration errors.
+ * Only the client side will have access to the window object.
+ * @returns (JSX.Element | null) - Modal Provider
+ */
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
 
+  // sets isMounted to true when the component is mounted (client side only).
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
+  // if the component is not mounted (server side) do not render the modal.
   if (!isMounted) {
     return null;
   }
