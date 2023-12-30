063d10db64b17d08922188204379f180611dd6f3
Maruf Bepary <bepary71@gmail.com>
2023-12-30 21:08:14 +0000

    Made design more modern - Added rounded corners - Added padding

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -153,14 +153,14 @@ export const Item = ({
         paddingLeft: level ? `${level * 12 + 12}px` : "12px",
       }}
       className={cn(
-        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        "group min-h-[30px] text-sm py-2 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-lg",
         active && "bg-primary/5 text-primary",
       )}
     >
       {!!id && (
         <div
           role="button"
-          className="h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
+          className="h-full rounded-lg hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
           onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
